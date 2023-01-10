a950012055c776bcee592cbd5121aef14a8169a8
Julio <aresden113@gmail.com>
2023-01-10 01:40:57 -0300

    card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.js'
-
-
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
+import CardProducts from './components/CardProducts/CardProducts.js';
 
 function App () {
   return (
-    
-    
-      <div className="App">
-        <Navbar />
-        <ItemListContainer greeting = 'Bienvenidos a Nuestro Shop'/>
+    <body>
+      <header>
+        <div className="App">
+          <Navbar />
+          <ItemListContainer greeting = 'Bienvenidos a Nuestro Shop'/>
+          
+        </div>
+      </header>
+      <div>
+          <CardProducts />
       </div>
+      
+
+    </body>
+    
+    
+    
     
     
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,44 @@
 import CartWidget from "../CartWidget/CartWidget"
 
+
 const Navbar = () => {
     return (
-        <nav>
-            <div classname='headsup'>
-                <img className="logolasgonzalez" src='./assest/img/lasgonzalez_tienda.png' />
-                
-            </div>
-            <div className="headinf">
-                <div>
-                <button className="botones">Quienes Somos</button>
-                <button className="botones">Como Comprar</button>
-                <button className="botones">Productos</button>
-                <button className="botones">Contactos</button>
+        <nav class="navbar navbar-expand-lg">
+                    <div classname='headsup'>
+                      <img className="logolasgonzalez" src='./assest/img/lasgonzalez_tienda.png' />
 
-                </div> 
-                <div>
-                <CartWidget />
+                    </div>
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                  <a class="nav-link active" href="">Como Comprar</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="">Productos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="">Contactos</a>
+                                </li>
+                                <li>
+                                    <a class="nav-link active" href="">Compras</a>
+                                </li>
+                                
+                                
+                            </ul>
+                            <div>
+                              <CartWidget />
                 
-                </div>           
-            </div>
-            
-        </nav>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
     )
 } 
 
